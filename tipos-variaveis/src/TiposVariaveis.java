dfc36f951b5661be6ac14b231d8b92c386fe4874
EssejOliveira <jesse.os@outlook.com>
2024-08-01 19:00:20 -0300

    tipos de variaveis 01/08/2024

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal; // esse caso dá erro pq a variável numeroCurto2 é do tipo short
+        // e está recebendo um int que tem a "capacidade" maior então nesse caso é recomendado o cast
+        //que ficaria assim -> short numeroCurto2 = (short)numeroNormal;
+
+        /**
+         * Para constantes a convenção adota a definição das constantes sempre em CAIXA_ALTA
+         * sendo precedida da palavra final
+         */
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println("Esse é o valor de PI: "+VALOR_DE_PI);
     }
 }
